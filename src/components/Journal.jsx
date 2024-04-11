7ceca1c333f3f83ed5fa4a31dcd03e9383d56cfc
Lasyasri Shilpi <shilpilasya@tamu.edu>
2024-04-11 11:54:12 -0500

    changes to profile

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Journal = () => {
+function Journal() {
   return (
     <div>
-      Journal
+      <h2>Journal</h2>
+      <p>This is the Journal page where users can write and view their journal entries.</p>
+      {/* Add more structure/functionality to your Journal page as needed */}
     </div>
-  )
+  );
 }
 
-export default Journal
+export default Journal;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "../styling/Profile.css"
+import logoImage from "../pictures/userprofile.png";
+import location from "../pictures/Location.png"
+
+const Profile = () => {
+  return (
+    <div className='Profile'>
+
+    <div class="logo">
+        <img src={logoImage} alt="logo" />
+    </div>  
+
+    <div className='user_name'>
+        <h1>Example User</h1>
+    </div>
+
+    <div className='user_location'>
+        <i class="fa-phone">07989 936719</i>
+        <h3>College Station, TX</h3>
+    </div>
+
+    </div>
+
+    
+  )
+}
+
+export default Profile
