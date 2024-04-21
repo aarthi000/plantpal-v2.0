d734f40960474eb236e226d935587e5d314aa7d9
“hdhulipala02” <hdhulipala02@tamu.edu>
2024-04-21 09:33:43 -0500

    forum title removed

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -6,7 +6,6 @@ import '../App.css'; // Importing the CSS file with correct path
 function Forum({ messages }) {
   return (
     <div className="forum">
-      <h2>Forum</h2>
       <div className="message-container">
         {messages.map((message, index) => (
           <div key={index} className="message-box">
